75a23e5950a778ca7ba84de887258359d0b8a650
riandi1 <rendondiaza944@gmail.com>
2023-09-18 12:53:09 -0500

    github pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,21 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
+import { styles } from '../styles'
+import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
-      <div
-        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
-      >
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
-          </h1>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hola, Soy <span className='text-[#915eff]'>Andres</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            Desarrollo interfaces de usuario, aplicaciones web <br className='sm:block hidden' />  y aplicaciones móviles como desarrollador Full Stack.
           </p>
         </div>
       </div>
@@ -46,6 +41,6 @@ const Hero = () => {
       </div>
     </section>
   );
-};
+}
 
 export default Hero;
